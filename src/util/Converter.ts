e407662690dab9edb9be7b54be166965c8afb48b
exoego <ytatsuno.jp@gmail.com>
2020-06-30 17:08:53 +0900

    Removed the unneeded object

--- src/util/Converter.ts
+++ src/util/Converter.ts
@@ -2,19 +2,6 @@ import * as numWords from 'num-words';
 
 
 export default class Converter {
-  private static CLASSNAME_REPLACEMENTS: { [key: string]: string; } = {
-    0: 'zero',
-    1: 'one',
-    2: 'two',
-    3: 'three',
-    4: 'four',
-    5: 'five',
-    6: 'six',
-    7: 'seven',
-    8: 'eight',
-    9: 'nine',
-  };
-
   private static NUMERIC_ONLY = /^\d+$/;
 
   public static iconClassName(name: string): string {
